e3d182eafd1a38b81bb594c482932dd746c1c81b
Maryan Serhii <sergijmara210@gmail.com>
2026-04-29 21:50:11 +0300

    some fix

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import 'dotenv/config';
-import cors from 'cors';
+
 import pino from 'pino-http';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+app.use(cors());
+
 app.use(express.json());
-app.use(cros());
+
 app.use(
   pino({
     level: 'info',
