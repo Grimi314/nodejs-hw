4e9fbb08df480c955630bfb5a95dee59d6611ccd
Maryan Serhii <sergijmara210@gmail.com>
2026-06-22 21:12:24 +0300

    6 fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -176,3 +176,36 @@ export const requestResetEmail = async (req, res, next) => {
     next(error);
   }
 };
+export const resetPassword = async (req, res, next) => {
+  try {
+    const { token, password } = req.body;
+
+    let payload;
+
+    try {
+      payload = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      throw createHttpError(401, 'Invalid or expired token');
+    }
+
+    const user = await User.findOne({
+      _id: payload.sub,
+      email: payload.email,
+    });
+
+    if (!user) {
+      throw createHttpError(404, 'User not found');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user.password = hashedPassword;
+    await user.save();
+
+    res.status(200).json({
+      message: 'Password reset successfully',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
